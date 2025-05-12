aef2599fb2bd9f8f2fb2e9db8ef3248d3cd88acd
Baghel004 <baghelghuman75@gmail.com>
2025-05-12 15:35:29 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with your frontend's origin if needed
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.options('*', (req, res) => {
+  res.sendStatus(200);
+});
+
 
 
 const together = new Together({ apiKey: process.env.SECRET_KEY });
